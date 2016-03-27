28b8fa939e02509cc9665839d3f7dd291faa776c
Eric Conlon <ejconlon@gmail.com>
2016-03-26 18:14:06 -0700

    mo

--- Induction.v
+++ Induction.v
@@ -1,1 +1,64 @@
 Require Export Basics.
+
+Require String. Open Scope string_scope.
+
+Ltac move_to_top x :=
+  match reverse goal with
+  | H : _ |- _ => try move x after H
+  end.
+
+Tactic Notation "assert_eq" ident(x) constr(v) :=
+  let H := fresh in
+  assert (x = v) as H by reflexivity;
+  clear H.
+
+Tactic Notation "Case_aux" ident(x) constr(name) :=
+  first [
+    set (x := name); move_to_top x
+  | assert_eq x name; move_to_top x
+  | fail 1 "because we are working on a different case" ].
+
+Tactic Notation "Case" constr(name) := Case_aux Case name.
+Tactic Notation "SCase" constr(name) := Case_aux SCase name.
+Tactic Notation "SSCase" constr(name) := Case_aux SSCase name.
+Tactic Notation "SSSCase" constr(name) := Case_aux SSSCase name.
+Tactic Notation "SSSSCase" constr(name) := Case_aux SSSSCase name.
+Tactic Notation "SSSSSCase" constr(name) := Case_aux SSSSSCase name.
+Tactic Notation "SSSSSSCase" constr(name) := Case_aux SSSSSSCase name.
+Tactic Notation "SSSSSSSCase" constr(name) := Case_aux SSSSSSSCase name.
+
+Theorem andb_true_elim1 : forall b c : bool,
+  andb b c = true -> b = true.
+Proof.
+  intros b c H.
+  destruct b.
+  Case "b = true".  (* <----- here *)
+    reflexivity.
+  Case "b = false".  (* <---- and here *)
+    rewrite <- H.
+    reflexivity.
+Qed.
+
+(** **** Exercise: 2 stars (andb_true_elim2) *)
+(** Prove [andb_true_elim2], marking cases (and subcases) when
+    you use [destruct]. *)
+
+Theorem andb_true_elim2 : forall b c : bool,
+  andb b c = true -> c = true.
+Proof.
+  intros b c.
+  destruct b.
+  Case "b = true".
+    rewrite andb_t_l.
+  intro H.
+  assumption.
+
+
+
+
+
+
+
+
+
+
--- Makefile
+++ Makefile
@@ -1,3 +1,7 @@
+.PHONY: clean all
+
+clean:
+	rm -f .*.aux *.glob *.vo
 
 all: Basics.vo Induction.vo
 
