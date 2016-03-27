20123190ac2b5a03191bfd7cdb40a792fe6532b0
Eric Conlon <ejconlon@gmail.com>
2016-03-27 10:35:25 -0700

    poly

--- Poly.v
+++ Poly.v
@@ -1,1 +1,168 @@
-Require Import Lists.
+Require Export Lists.
+
+Inductive list (X:Type) : Type :=
+  | nil : list X
+  | cons : X -> list X -> list X.
+
+Fixpoint length (X:Type) (l:list X) : nat :=
+  match l with
+  | nil _ => 0
+  | cons _ h t => S (length X t)
+  end.
+
+Example test_length1 :
+    length nat (cons nat 1 (cons nat 2 (nil nat))) = 2.
+Proof. reflexivity.  Qed.
+
+Example test_length2 :
+    length bool (cons bool true (nil bool)) = 1.
+Proof. reflexivity.  Qed.
+
+Fixpoint app (X : Type) (l1 l2 : list X)
+                : (list X) :=
+  match l1 with
+  | nil _     => l2
+  | cons _ h t => cons X h (app X t l2)
+  end.
+
+Fixpoint snoc (X:Type) (l:list X) (v:X) : (list X) :=
+  match l with
+  | nil _      => cons X v (nil X)
+  | cons _ h t => cons X h (snoc X t v)
+  end.
+
+Fixpoint rev (X:Type) (l:list X) : list X :=
+  match l with
+  | nil _      => nil X
+  | cons _ h t => snoc X (rev X t) h
+  end.
+
+Example test_rev1 :
+    rev nat (cons nat 1 (cons nat 2 (nil nat)))
+  = (cons nat 2 (cons nat 1 (nil nat))).
+Proof. reflexivity.  Qed.
+
+Example test_rev2:
+  rev bool (nil bool) = nil bool.
+Proof. reflexivity.  Qed.
+
+Arguments nil {X}.
+Arguments cons {X} _ _.  (* use underscore for argument position that has no name *)
+Arguments length {X} l.
+Arguments app {X} l1 l2.
+Arguments rev {X} l. 
+Arguments snoc {X} l v.
+
+Notation "x :: y" := (cons x y)
+                     (at level 60, right associativity).
+Notation "[ ]" := nil.
+Notation "[ x ; .. ; y ]" := (cons x .. (cons y []) ..).
+Notation "x ++ y" := (app x y)
+                     (at level 60, right associativity).
+
+Fixpoint repeat {X : Type} (n : X) (count : nat) : list X :=
+  match count with
+  | 0 => []
+  | S count' => n :: (repeat n count')
+  end.
+
+Example test_repeat1:
+  repeat true 2 = cons true (cons true nil).
+Proof. reflexivity. Qed.
+
+Theorem nil_app : forall X:Type, forall l:list X,
+  app [] l = l.
+Proof. reflexivity. Qed.
+
+Theorem rev_snoc : forall X : Type,
+                     forall v : X,
+                     forall s : list X,
+  rev (snoc s v) = v :: (rev s).
+Proof.
+  intros.
+  induction s as [| n t].
+  reflexivity.
+  simpl.
+  rewrite IHt.
+  reflexivity.
+Qed.
+
+Theorem rev_involutive : forall X : Type, forall l : list X,
+  rev (rev l) = l.
+Proof.
+  intros.
+  induction l as [| n t].
+  reflexivity.
+  simpl.
+  rewrite rev_snoc.
+  rewrite IHt.
+  reflexivity.
+Qed.
+
+Theorem snoc_with_append : forall X : Type,
+                         forall l1 l2 : list X,
+                         forall v : X,
+  snoc (l1 ++ l2) v = l1 ++ (snoc l2 v).
+Proof.
+  intros. induction l1 as [|v' l1'].
+  reflexivity.
+  simpl.
+  rewrite IHl1'.
+  reflexivity.
+Qed.
+
+Inductive prod (X Y : Type) : Type :=
+  pair : X -> Y -> prod X Y.
+
+Arguments pair {X} {Y} _ _.
+
+Notation "( x , y )" := (pair x y).
+
+Notation "X * Y" := (prod X Y) : type_scope.
+
+Definition fst {X Y : Type} (p : X * Y) : X :=
+  match p with (x,y) => x end.
+
+Definition snd {X Y : Type} (p : X * Y) : Y :=
+  match p with (x,y) => y end.
+
+Fixpoint combine {X Y : Type} (lx : list X) (ly : list Y)
+           : list (X*Y) :=
+  match (lx,ly) with
+  | ([],_) => []
+  | (_,[]) => []
+  | (x::tx, y::ty) => (x,y) :: (combine tx ty)
+  end.
+
+(** **** Exercise: 1 star, optional (combine_checks) *)
+(** Try answering the following questions on paper and
+    checking your answers in coq:
+    - What is the type of [combine] (i.e., what does [Check
+      @combine] print?)
+    - What does
+        Eval compute in (combine [1;2] [false;false;true;true]).
+      print?   []
+*)
+
+Check @combine.
+
+(** **** Exercise: 2 stars (split) *)
+(** The function [split] is the right inverse of combine: it takes a
+    list of pairs and returns a pair of lists.  In many functional
+    programing languages, this function is called [unzip].
+
+    Uncomment the material below and fill in the definition of
+    [split].  Make sure it passes the given unit tests. *)
+
+Fixpoint split
+           {X Y : Type} (l : list (X*Y))
+           : (list X) * (list Y) :=
+(* FILL IN HERE *) admit.
+
+Example test_split:
+  split [(1,false);(2,false)] = ([1;2],[false;false]).
+Proof.
+(* FILL IN HERE *) Admitted.
+(** [] *)
+
+
