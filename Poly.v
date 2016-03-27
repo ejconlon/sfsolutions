327f10d9c257b69f978cc27fe289ac38f0ae527f
Eric Conlon <ejconlon@gmail.com>
2016-03-27 10:56:25 -0700

    more poly

--- Poly.v
+++ Poly.v
@@ -134,35 +134,467 @@ Fixpoint combine {X Y : Type} (lx : list X) (ly : list Y)
   | (x::tx, y::ty) => (x,y) :: (combine tx ty)
   end.
 
-(** **** Exercise: 1 star, optional (combine_checks) *)
-(** Try answering the following questions on paper and
-    checking your answers in coq:
-    - What is the type of [combine] (i.e., what does [Check
-      @combine] print?)
-    - What does
-        Eval compute in (combine [1;2] [false;false;true;true]).
-      print?   []
-*)
+Fixpoint subsplit {X Y : Type} (l : list (X*Y)) (xs: list X) (ys: list Y)
+           : (list X) * (list Y) :=
+  match l with
+  | [] => (xs, ys)
+  | (x, y) :: xys => subsplit xys (x :: xs) (y :: ys)
+  end.
 
-Check @combine.
-
-(** **** Exercise: 2 stars (split) *)
-(** The function [split] is the right inverse of combine: it takes a
-    list of pairs and returns a pair of lists.  In many functional
-    programing languages, this function is called [unzip].
-
-    Uncomment the material below and fill in the definition of
-    [split].  Make sure it passes the given unit tests. *)
-
-Fixpoint split
+Definition split
            {X Y : Type} (l : list (X*Y))
            : (list X) * (list Y) :=
-(* FILL IN HERE *) admit.
+  match subsplit l [] [] with
+  | (xs, ys) => (rev xs, rev ys)
+  end.
 
 Example test_split:
   split [(1,false);(2,false)] = ([1;2],[false;false]).
+Proof. reflexivity. Qed.
+
+Inductive option (X:Type) : Type :=
+  | Some : X -> option X
+  | None : option X.
+
+Arguments Some {X} _. 
+Arguments None {X}. 
+
+Fixpoint index {X : Type} (n : nat)
+               (l : list X) : option X :=
+  match l with
+  | [] => None
+  | a :: l' => if beq_nat n O then Some a else index (pred n) l'
+  end.
+
+Example test_index1 :    index 0 [4;5;6;7]  = Some 4.
+Proof. reflexivity.  Qed.
+Example test_index2 :    index  1 [[1];[2]]  = Some [2].
+Proof. reflexivity.  Qed.
+Example test_index3 :    index  2 [true]  = None.
+Proof. reflexivity.  Qed.
+
+Definition hd_opt {X : Type} (l : list X)  : option X :=
+  match l with
+  | [] => None
+  | v :: _ => Some v
+  end.
+
+Example test_hd_opt1 :  hd_opt [1;2] = Some 1.
+Proof. reflexivity. Qed.
+Example test_hd_opt2 :   hd_opt  [[1];[2]]  = Some [1].
+Proof. reflexivity. Qed.
+
+Definition doit3times {X:Type} (f:X->X) (n:X) : X :=
+  f (f (f n)).
+
+Example test_doit3times': doit3times negb true = false.
+Proof. reflexivity.  Qed.
+
+Definition plus3 := plus 3.
+
+Example test_plus3 :    plus3 4 = 7.
+Proof. reflexivity.  Qed.
+Example test_plus3' :   doit3times plus3 0 = 9.
+Proof. reflexivity.  Qed.
+Example test_plus3'' :  doit3times (plus 3) 0 = 9.
+Proof. reflexivity.  Qed.
+
+Definition prod_curry {X Y Z : Type}
+  (f : X * Y -> Z) (x : X) (y : Y) : Z := f (x, y).
+
+Definition prod_uncurry {X Y Z : Type}
+  (f : X -> Y -> Z) (p : X * Y) : Z :=
+  match p with
+  | (x, y) => f x y
+  end.
+
+Theorem uncurry_curry : forall (X Y Z : Type) (f : X -> Y -> Z) x y,
+  prod_curry (prod_uncurry f) x y = f x y.
+Proof. reflexivity. Qed.
+
+Theorem curry_uncurry : forall (X Y Z : Type)
+                               (f : (X * Y) -> Z) (p : X * Y),
+  prod_uncurry (prod_curry f) p = f p.
 Proof.
-(* FILL IN HERE *) Admitted.
+  intros.
+  destruct p as [m n].
+  reflexivity.
+Qed.
+
+Fixpoint filter {X:Type} (test: X->bool) (l:list X)
+                : (list X) :=
+  match l with
+  | []     => []
+  | h :: t => if test h then h :: (filter test t)
+                        else       filter test t
+  end.
+
+Example test_filter1: filter evenb [1;2;3;4] = [2;4].
+Proof. reflexivity.  Qed.
+
+Definition length_is_1 {X : Type} (l : list X) : bool :=
+  beq_nat (length l) 1.
+
+Example test_filter2:
+    filter length_is_1
+           [ [1; 2]; [3]; [4]; [5;6;7]; []; [8] ]
+  = [ [3]; [4]; [8] ].
+Proof. reflexivity.  Qed.
+
+Definition countoddmembers' (l:list nat) : nat :=
+  length (filter oddb l).
+
+Example test_countoddmembers'1:   countoddmembers' [1;0;3;1;4;5] = 4.
+Proof. reflexivity.  Qed.
+Example test_countoddmembers'2:   countoddmembers' [0;2;4] = 0.
+Proof. reflexivity.  Qed.
+Example test_countoddmembers'3:   countoddmembers' nil = 0.
+Proof. reflexivity.  Qed.
+
+Example test_anon_fun':
+  doit3times (fun n => n * n) 2 = 256.
+Proof. reflexivity.  Qed.
+
+Example test_filter2':
+    filter (fun l => beq_nat (length l) 1)
+           [ [1; 2]; [3]; [4]; [5;6;7]; []; [8] ]
+  = [ [3]; [4]; [8] ].
+Proof. reflexivity.  Qed.
+
+(** **** Exercise: 2 stars (filter_even_gt7) *)
+
+(** Use [filter] (instead of [Fixpoint]) to write a Coq function
+    [filter_even_gt7] that takes a list of natural numbers as input
+    and returns a list of just those that are even and greater than
+    7. *)
+
+Definition filter_even_gt7 (l : list nat) : list nat :=
+  filter (fun i => andb (evenb i) (blt_nat 7 i)) l.
+
+Example test_filter_even_gt7_1 :
+  filter_even_gt7 [1;2;6;9;10;3;12;8] = [10;12;8].
+Proof. reflexivity. Qed.
+
+Example test_filter_even_gt7_2 :
+  filter_even_gt7 [5;2;6;19;129] = [].
+Proof. reflexivity. Qed.
+
+Definition partition {X : Type} (f: X -> bool) (l: list X) : list X * list X :=
+  (filter f l, filter (fun v => negb (f v)) l).
+
+Example test_partition1: partition oddb [1;2;3;4;5] = ([1;3;5], [2;4]).
+Proof. reflexivity. Qed.
+Example test_partition2: partition (fun x => false) [5;9;0] = ([], [5;9;0]).
+Proof. reflexivity. Qed.
+
+(* ###################################################### *)
+(** ** Map *)
+
+(** Another handy higher-order function is called [map]. *)
+
+Fixpoint map {X Y:Type} (f:X->Y) (l:list X)
+             : (list Y) :=
+  match l with
+  | []     => []
+  | h :: t => (f h) :: (map f t)
+  end.
+
+(** *** *)
+(** It takes a function [f] and a list [ l = [n1, n2, n3, ...] ]
+    and returns the list [ [f n1, f n2, f n3,...] ], where [f] has
+    been applied to each element of [l] in turn.  For example: *)
+
+Example test_map1: map (plus 3) [2;0;2] = [5;3;5].
+Proof. reflexivity.  Qed.
+
+(** The element types of the input and output lists need not be
+    the same ([map] takes _two_ type arguments, [X] and [Y]).  This
+    version of [map] can thus be applied to a list of numbers and a
+    function from numbers to booleans to yield a list of booleans: *)
+
+Example test_map2: map oddb [2;1;2;5] = [false;true;false;true].
+Proof. reflexivity.  Qed.
+
+(** It can even be applied to a list of numbers and
+    a function from numbers to _lists_ of booleans to
+    yield a list of lists of booleans: *)
+
+Example test_map3:
+    map (fun n => [evenb n;oddb n]) [2;1;2;5]
+  = [[true;false];[false;true];[true;false];[false;true]].
+Proof. reflexivity.  Qed.
+
+
+
+(** ** Map for options *)
+(** **** Exercise: 3 stars (map_rev) *)
+(** Show that [map] and [rev] commute.  You may need to define an
+    auxiliary lemma. *)
+
+
+Theorem map_rev : forall (X Y : Type) (f : X -> Y) (l : list X),
+  map f (rev l) = rev (map f l).
+Proof.
+  (* FILL IN HERE *) Admitted.
 (** [] *)
 
+(** **** Exercise: 2 stars (flat_map) *)
+(** The function [map] maps a [list X] to a [list Y] using a function
+    of type [X -> Y].  We can define a similar function, [flat_map],
+    which maps a [list X] to a [list Y] using a function [f] of type
+    [X -> list Y].  Your definition should work by 'flattening' the
+    results of [f], like so:
+        flat_map (fun n => [n;n+1;n+2]) [1;5;10]
+      = [1; 2; 3; 5; 6; 7; 10; 11; 12].
+*)
+
+Fixpoint flat_map {X Y:Type} (f:X -> list Y) (l:list X)
+                   : (list Y) :=
+  (* FILL IN HERE *) admit.
+
+Example test_flat_map1:
+  flat_map (fun n => [n;n;n]) [1;5;4]
+  = [1; 1; 1; 5; 5; 5; 4; 4; 4].
+ (* FILL IN HERE *) Admitted.
+(** [] *)
+
+(** Lists are not the only inductive type that we can write a
+    [map] function for.  Here is the definition of [map] for the
+    [option] type: *)
+
+Definition option_map {X Y : Type} (f : X -> Y) (xo : option X)
+                      : option Y :=
+  match xo with
+    | None => None
+    | Some x => Some (f x)
+  end.
+
+(** **** Exercise: 2 stars, optional (implicit_args) *)
+(** The definitions and uses of [filter] and [map] use implicit
+    arguments in many places.  Replace the curly braces around the
+    implicit arguments with parentheses, and then fill in explicit
+    type parameters where necessary and use Coq to check that you've
+    done so correctly.  (This exercise is not to be turned in; it is
+    probably easiest to do it on a _copy_ of this file that you can
+    throw away afterwards.)  [] *)
+
+(* ###################################################### *)
+(** ** Fold *)
+
+(** An even more powerful higher-order function is called
+    [fold].  This function is the inspiration for the "[reduce]"
+    operation that lies at the heart of Google's map/reduce
+    distributed programming framework. *)
+
+Fixpoint fold {X Y:Type} (f: X->Y->Y) (l:list X) (b:Y) : Y :=
+  match l with
+  | nil => b
+  | h :: t => f h (fold f t b)
+  end.
+
+(** *** *)
+
+(** Intuitively, the behavior of the [fold] operation is to
+    insert a given binary operator [f] between every pair of elements
+    in a given list.  For example, [ fold plus [1;2;3;4] ] intuitively
+    means [1+2+3+4].  To make this precise, we also need a "starting
+    element" that serves as the initial second input to [f].  So, for
+    example,
+   fold plus [1;2;3;4] 0
+    yields
+   1 + (2 + (3 + (4 + 0))).
+    Here are some more examples:
+*)
+
+Check (fold andb).
+(* ===> fold andb : list bool -> bool -> bool *)
+
+Example fold_example1 : fold mult [1;2;3;4] 1 = 24.
+Proof. reflexivity. Qed.
+
+Example fold_example2 : fold andb [true;true;false;true] true = false.
+Proof. reflexivity. Qed.
+
+Example fold_example3 : fold app  [[1];[];[2;3];[4]] [] = [1;2;3;4].
+Proof. reflexivity. Qed.
+
+
+(** **** Exercise: 1 star, advanced (fold_types_different) *)
+(** Observe that the type of [fold] is parameterized by _two_ type
+    variables, [X] and [Y], and the parameter [f] is a binary operator
+    that takes an [X] and a [Y] and returns a [Y].  Can you think of a
+    situation where it would be useful for [X] and [Y] to be
+    different? *)
+
+(* ###################################################### *)
+(** ** Functions For Constructing Functions *)
+
+(** Most of the higher-order functions we have talked about so
+    far take functions as _arguments_.  Now let's look at some
+    examples involving _returning_ functions as the results of other
+    functions.
+
+    To begin, here is a function that takes a value [x] (drawn from
+    some type [X]) and returns a function from [nat] to [X] that
+    yields [x] whenever it is called, ignoring its [nat] argument. *)
+
+Definition constfun {X: Type} (x: X) : nat->X :=
+  fun (k:nat) => x.
+
+Definition ftrue := constfun true.
+
+Example constfun_example1 : ftrue 0 = true.
+Proof. reflexivity. Qed.
+
+Example constfun_example2 : (constfun 5) 99 = 5.
+Proof. reflexivity. Qed.
+
+(** *** *)
+(** Similarly, but a bit more interestingly, here is a function
+    that takes a function [f] from numbers to some type [X], a number
+    [k], and a value [x], and constructs a function that behaves
+    exactly like [f] except that, when called with the argument [k],
+    it returns [x]. *)
+
+Definition override {X: Type} (f: nat->X) (k:nat) (x:X) : nat->X:=
+  fun (k':nat) => if beq_nat k k' then x else f k'.
+
+(** For example, we can apply [override] twice to obtain a
+    function from numbers to booleans that returns [false] on [1] and
+    [3] and returns [true] on all other arguments. *)
+
+Definition fmostlytrue := override (override ftrue 1 false) 3 false.
+
+(** *** *)
+
+Example override_example1 : fmostlytrue 0 = true.
+Proof. reflexivity. Qed.
+
+Example override_example2 : fmostlytrue 1 = false.
+Proof. reflexivity. Qed.
+
+Example override_example3 : fmostlytrue 2 = true.
+Proof. reflexivity. Qed.
+
+Example override_example4 : fmostlytrue 3 = false.
+Proof. reflexivity. Qed.
+
+(** *** *)
+
+(** **** Exercise: 1 star (override_example) *)
+(** Before starting to work on the following proof, make sure you
+    understand exactly what the theorem is saying and can paraphrase
+    it in your own words.  The proof itself is straightforward. *)
+
+Theorem override_example : forall (b:bool),
+  (override (constfun b) 3 true) 2 = b.
+Proof.
+  (* FILL IN HERE *) Admitted.
+(** [] *)
+
+(** We'll use function overriding heavily in parts of the rest of the
+    course, and we will end up needing to know quite a bit about its
+    properties.  To prove these properties, though, we need to know
+    about a few more of Coq's tactics; developing these is the main
+    topic of the next chapter.  For now, though, let's introduce just
+    one very useful tactic that will also help us with proving
+    properties of some of the other functions we have introduced in
+    this chapter. *)
+
+(* ###################################################### *)
+
+(* ###################################################### *)
+(** * The [unfold] Tactic *)
+
+(** Sometimes, a proof will get stuck because Coq doesn't
+    automatically expand a function call into its definition.  (This
+    is a feature, not a bug: if Coq automatically expanded everything
+    possible, our proof goals would quickly become enormous -- hard to
+    read and slow for Coq to manipulate!) *)
+
+Theorem unfold_example_bad : forall m n,
+  3 + n = m ->
+  plus3 n + 1 = m + 1.
+Proof.
+  intros m n H.
+(* At this point, we'd like to do [rewrite -> H], since 
+     [plus3 n] is definitionally equal to [3 + n].  However, 
+     Coq doesn't automatically expand [plus3 n] to its 
+     definition. *)
+  Abort.
+
+(** The [unfold] tactic can be used to explicitly replace a
+    defined name by the right-hand side of its definition.  *)
+
+Theorem unfold_example : forall m n,
+  3 + n = m ->
+  plus3 n + 1 = m + 1.
+Proof.
+  intros m n H.
+  unfold plus3.
+  rewrite -> H.
+  reflexivity.  Qed.
+
+(** Now we can prove a first property of [override]: If we
+    override a function at some argument [k] and then look up [k], we
+    get back the overridden value. *)
+
+Theorem override_eq : forall {X:Type} x k (f:nat->X),
+  (override f k x) k = x.
+Proof.
+  intros X x k f.
+  unfold override.
+  rewrite <- beq_nat_refl.
+  reflexivity.  Qed.
+
+(** This proof was straightforward, but note that it requires
+    [unfold] to expand the definition of [override]. *)
+
+(** **** Exercise: 2 stars (override_neq) *)
+Theorem override_neq : forall (X:Type) x1 x2 k1 k2 (f : nat->X),
+  f k1 = x1 ->
+  beq_nat k2 k1 = false ->
+  (override f k2 x2) k1 = x1.
+Proof.
+  (* FILL IN HERE *) Admitted.
+(** [] *)
+
+(** As the inverse of [unfold], Coq also provides a tactic
+    [fold], which can be used to "unexpand" a definition.  It is used
+    much less often. *)
+
+(* ##################################################### *)
+(** * Additional Exercises *)
+
+(** **** Exercise: 2 stars (fold_length) *)
+(** Many common functions on lists can be implemented in terms of
+   [fold].  For example, here is an alternative definition of [length]: *)
+
+Definition fold_length {X : Type} (l : list X) : nat :=
+  fold (fun _ n => S n) l 0.
+
+Example test_fold_length1 : fold_length [4;7;0] = 3.
+Proof. reflexivity. Qed.
+
+(** Prove the correctness of [fold_length]. *)
+
+Theorem fold_length_correct : forall X (l : list X),
+  fold_length l = length l.
+(* FILL IN HERE *) Admitted. 
+(** [] *)
+
+(** **** Exercise: 3 stars (fold_map) *)
+(** We can also define [map] in terms of [fold].  Finish [fold_map]
+    below. *)
+
+Definition fold_map {X Y:Type} (f : X -> Y) (l : list X) : list Y :=
+(* FILL IN HERE *) admit.
+
+(** Write down a theorem in Coq stating that [fold_map] is correct,
+    and prove it. *)
+
+(* FILL IN HERE *)
+(** [] *)
+
+(* $Date: 2013-09-26 14:40:26 -0400 (Thu, 26 Sep 2013) $ *)
 
