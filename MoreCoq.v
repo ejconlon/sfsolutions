99d2f3efb2c2c4d42cbb5f5bd12c50c1aa6a4ad6
Eric Conlon <ejconlon@gmail.com>
2016-03-27 19:59:00 -0700

    fixit

--- MoreCoq.v
+++ MoreCoq.v
@@ -1,2 +1,23 @@
 Require Export Poly.
 
+Theorem silly_ex : 
+     (forall n, evenb n = true -> oddb (S n) = true) ->
+     evenb 3 = true ->
+     oddb 4 = true.
+Proof.
+  intros H.
+  apply H.
+Qed.
+
+Theorem rev_exercise1 : forall (l l' : list nat),
+     l = rev l' ->
+     l' = rev l.
+Proof.
+  intros.
+  assert (H0: l' = rev (rev l')).
+    symmetry.
+    apply rev_involutive.
+  rewrite H.
+  symmetry.  
+  apply rev_involutive.
+Qed.
